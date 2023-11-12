e9f7f9956b2693401aaefaaab50e4e5c92c0697f
zxmboshi <zxmboshi@hust.edu.cn>
2023-11-12 18:19:35 +0800

    two stream batch sampler

---
+++ dataloaders/util.py
@@ -0,0 +1,152 @@
+import torch
+import numpy as np
+import itertools
+from torch.utils.data.sampler import Sampler
+
+
+class CenterCrop:
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        # pad the sample if necessary
+        if label.shape[0] <= self.output_size[0] or label.shape[1] <= self.output_size[1] or label.shape[2] <= \
+                self.output_size[2]:
+            pw = max((self.output_size[0] - label.shape[0]) // 2 + 3, 0)
+            ph = max((self.output_size[1] - label.shape[1]) // 2 + 3, 0)
+            pd = max((self.output_size[2] - label.shape[2]) // 2 + 3, 0)
+            image = np.pad(image, [(pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
+            label = np.pad(label, [(pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
+            
+        w, h, d = image.shape
+
+        w1 = int(round((w - self.output_size[0]) / 2.))
+        h1 = int(round((h - self.output_size[1]) / 2.))
+        d1 = int(round((d - self.output_size[2]) / 2.))
+
+        label = label[w1:w1 + self.output_size[0], h1:h1 + self.output_size[1], d1:d1 + self.output_size[2]]
+        image = image[w1:w1 + self.output_size[0], h1:h1 + self.output_size[1], d1:d1 + self.output_size[2]]
+
+        return {'image': image, 'label': label}
+
+
+class RandomCrop:
+    """
+    Crop randomly the image in a sample
+    """
+
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        # pad the sample if necessary
+        if label.shape[0] <= self.output_size[0] or label.shape[1] <= self.output_size[1] or label.shape[2] <= \
+                self.output_size[2]:
+            pw = max((self.output_size[0] - label.shape[0]) // 2 + 3, 0)
+            ph = max((self.output_size[1] - label.shape[1]) // 2 + 3, 0)
+            pd = max((self.output_size[2] - label.shape[2]) // 2 + 3, 0)
+            image = np.pad(image, [(pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
+            label = np.pad(label, [(pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
+                
+        w, h, d = image.shape
+        
+        w1 = np.random.randint(0, w - self.output_size[0])
+        h1 = np.random.randint(0, h - self.output_size[1])
+        d1 = np.random.randint(0, d - self.output_size[2])
+
+        label = label[w1:w1 + self.output_size[0], h1:h1 + self.output_size[1], d1:d1 + self.output_size[2]]
+        image = image[w1:w1 + self.output_size[0], h1:h1 + self.output_size[1], d1:d1 + self.output_size[2]]
+        
+        return {'image': image, 'label': label}
+
+
+class RandomRotFlip:
+    """
+    Randomly rotate and flip the dataset in a sample
+    """
+    
+    def __init__(self, p=0.5):
+        self.prob = p
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        if np.random.rand() < self.prob:
+            k = np.random.randint(0, 4)
+            image = np.rot90(image, k)
+            label = np.rot90(label, k)
+        if np.random.rand() < self.prob:
+            axis = np.random.randint(0, 3)
+            image = np.flip(image, axis=axis)
+            label = np.flip(label, axis=axis)
+        return {'image': image.copy(), 'label': label.copy()}
+
+
+class RandomNoise:
+    def __init__(self, mu=0, sigma=0.1):
+        self.mu = mu
+        self.sigma = sigma
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        noise = np.clip(self.sigma * np.random.randn(
+            image.shape[0], image.shape[1], image.shape[2]), -2*self.sigma, 2*self.sigma)
+        noise = noise + self.mu
+        image = image + noise
+        return {'image': image, 'label': label}
+
+
+class ToTensor:
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        return {'image': torch.from_numpy(image[None]), 'label': torch.from_numpy(label).long()}
+
+
+class TwoStreamBatchSampler(Sampler):
+    """Iterate two sets of indices
+
+    An 'epoch' is one iteration through the primary indices.
+    During the epoch, the secondary indices are iterated through
+    as many times as needed.
+    """
+    def __init__(self, primary_indices, secondary_indices, batch_size, secondary_batch_size):
+        self.primary_indices = primary_indices
+        self.secondary_indices = secondary_indices
+        self.secondary_batch_size = secondary_batch_size
+        self.primary_batch_size = batch_size - secondary_batch_size
+
+        assert len(self.primary_indices) >= self.primary_batch_size > 0
+        assert len(self.secondary_indices) >= self.secondary_batch_size > 0
+
+    def __iter__(self):
+        primary_iter = iterate_once(self.primary_indices)
+        secondary_iter = iterate_eternally(self.secondary_indices)
+        return (
+            primary_batch + secondary_batch
+            for (primary_batch, secondary_batch)
+            in zip(grouper(primary_iter, self.primary_batch_size),
+                    grouper(secondary_iter, self.secondary_batch_size))
+        )
+
+    def __len__(self):
+        return len(self.primary_indices) // self.primary_batch_size
+
+def iterate_once(iterable):
+    return np.random.permutation(iterable)
+
+
+def iterate_eternally(indices):
+    def infinite_shuffles():
+        while True:
+            yield np.random.permutation(indices)
+    return itertools.chain.from_iterable(infinite_shuffles())
+
+
+def grouper(iterable, n):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3) --> ABC DEF"
+    args = [iter(iterable)] * n
+    return zip(*args)
